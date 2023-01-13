1e53435fd5fc09370c436e63fa63ce2075d2b9f5
Itamar <itamarportela@gmail.com>
2023-01-13 15:20:11 -0300

    English localization, removing data from while

--- print.py
+++ print.py
@@ -9,5 +9,5 @@ while(True):
 	screenshot=pyautogui.screenshot(region) #Screen area registered, in pixels
 	screenshot2=screenshot.crop(size) #Image size, in pixels
 	screenshot2.save(savedir+local.strftime("%m%d%Y_%H%M%S")+'.png') #defining save directory, extension and saving image
-	print('Salvando: '+savedir+local.strftime("%m%d%Y_%H%M%S")+'.png')
+	print('Saving: '+savedir+local.strftime("%m%d%Y_%H%M%S")+'.png')
 	time.sleep(interval) # screenshot interval
